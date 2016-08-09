7fd35465b316c466ee9187c7691f177f24b9db08
Lee1984 <toastytraxx@hotmail.com>
2016-08-09 17:39:20 +1000

    Added some_sort_of_code update

--- omniauth.rb
+++ omniauth.rb
@@ -3,3 +3,5 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, 'fb id', 'secret', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
 end
+some_sort_of_code
+end
